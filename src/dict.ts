06af2beb05d35e93dcbd86ff8b9ee89ed7f761a9
Michael Bayne <mdb@samskivert.com>
2024-01-21 16:36:53 -0800

    Let's name that something more obvious.

--- src/dict.ts
+++ src/dict.ts
@@ -1,4 +1,4 @@
-import { encodedWords } from "./words"
+import { encodedWords } from "./encoded-words"
 
 export const words = new Set<string>()
 const digits = new Set(["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"])
